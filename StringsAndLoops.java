5d09edf4a55b7aa5932a9b65bf9a6427d699a6b8
byuheng <141770040+byuheng@users.noreply.github.com>
2023-08-22 12:03:59 -0700

    Add files via upload

---
+++ StringsAndLoops.java
@@ -0,0 +1,26 @@
+
+public class StringsAndLoops {
+    public static void main(String[] args) {
+
+        System.out.println("\nWelcome to Strings and Loops!\n");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 20; i++) {
+            System.out.println("i = " + i);
+        }
+
+        int myLoopControlVariable = 0;
+
+        while (myLoopControlVariable <= 12){
+            System.out.println("myLoopControlVariable is..." + myLoopControlVariable);
+            myLoopControlVariable++;
+        }
+
+        String[] myStrArray = {"this", "that", "the other"};
+        int[] myIntegerArray = {22, 33, 12, 15, 7, 1};
+
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("myIntegerArray[" + i + "] = " + myIntegerArray[i]);
+        }
+    }
+}
